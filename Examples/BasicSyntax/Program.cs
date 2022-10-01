9912a8f15c558392377763dc427dfdf472748eb1
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-01 23:19:40 +0600

    Example of ReadLine(), ReadKey(), Read() Method

--- Examples/BasicSyntax/Program.cs
+++ Examples/BasicSyntax/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /***
             Console.WriteLine("Hello World");
             // Variable can have alphabet, digit ot undrescore and it must start with either alphabet or underscore
             const int studentAge = 23;
@@ -24,6 +25,48 @@
             {
                 Console.WriteLine(lineParts[i]);
             }
+            ***/
+
+            // ****************************************
+            // ****** C# Basic Input and Output *******
+            // ****************************************
+
+            // Example 4: Printing Concatenated String using + operator
+
+            /***
+            int val = 55;
+            Console.WriteLine("Hello " + "World");
+            Console.WriteLine("Value = " + val);
+            ***/
+
+            // *****************************
+            // ****** C# Basic Input *******
+            // *****************************
+
+            // In C#, the simplest method to get input from the user is by using the ReadLine() Method of the Console Class. However, Read() and ReadKey() are also available for getting input from the user. They are also included in console class.
+
+            // Example 6:Get String Input From User
+
+            /***
+            string testString;
+            Console.Write("Enter a string - ");
+            testString = Console.ReadLine();
+            Console.WriteLine("You Entered: '{0}'", testString);
+            ***/
+
+            // Example 7: Difference between Read() and ReadKey() method
+            
+            int userInput;
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.Write("Input using Read() - ");
+            userInput = Console.Read();
+            Console.WriteLine(userInput);
+
+           
         }
     }
 }
