79b5c30a1579803890d11aa98ab1ed55a27bd7d3
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-01 11:51:48 +0600

    Basic Assignment Operator

---
+++ Examples/Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *****************************
+            // ******** C# Operators *******
+            // *****************************
+
+            // Example 1: Basic Assignment Operator
+            
+            int firstNumber, secondNumber;
+            // Assigning a constant to a variable
+            firstNumber = 10;
+            Console.WriteLine("First Number = {0}", firstNumber);
+
+            // Assigning a variable to another variable
+            secondNumber = firstNumber;
+            Console.WriteLine("Second Number = {0}", secondNumber);
+        }
+    }
+}
