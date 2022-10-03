ecfeaeea30a7bc3e6f3612628be7c09ed76a4117
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-03 12:03:45 +0600

    Example of C# if Statement

---
+++ Examples/Flow Control/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Flow_Control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *********************************************************************
+            // ****** C# if, if...else, if...else if and Nested if Statement *******
+            // *********************************************************************
+
+            // C# if (if-then) Statement
+            // -------------------------
+
+            // Example 1: C# if Statement
+
+            int number = 2;
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+
+            Console.WriteLine("This statement is always executed.");
+        }
+    }
+}
