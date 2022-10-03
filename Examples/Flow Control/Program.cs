06459b54b9150b3c491a0216c2981c783fd44b4c
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-03 16:45:34 +0600

    Example of C# if...else if Statement

--- Examples/Flow Control/Program.cs
+++ Examples/Flow Control/Program.cs
@@ -13,6 +13,7 @@
 
             // Example 1: C# if Statement
 
+            /***
             int number = 2;
             if (number < 5)
             {
@@ -20,6 +21,49 @@
             }
 
             Console.WriteLine("This statement is always executed.");
+            ***/
+
+            // C# if...else (if-then-else) Statement
+            // -------------------------------------
+
+            // Example 2: C# if...else Statement
+
+            /***
+            int number = 12;
+
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater than or equal to 5", number);
+            }
+
+            Console.WriteLine("This program is always executed");
+            ***/
+
+            // C# if...else if (if-then-else if) Statement
+            // -------------------------------------------
+
+            // Example 3: C# if...else if Statement
+            
+            int number = 12;
+
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+            else if(number > 5)
+            {
+                Console.WriteLine("{0} is greater than 5", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is equal to 5", number);
+            }
+
+
         }
     }
 }
