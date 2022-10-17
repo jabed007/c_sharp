da3ceccabd45e3333efa35cf7c42a4b88686fe40
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-17 16:45:11 +0600

    Math operators

--- Examples/Operators/Program.cs
+++ Examples/Operators/Program.cs
@@ -252,6 +252,54 @@
             Console.WriteLine(-4); 	// output: -4
             Console.WriteLine(-(-4));  // output: 4
 
+            // Math operators
+            Console.WriteLine(5 * 2); // output: 10
+            Console.WriteLine(0.5 * 2.5); // output: 1.25
+            Console.WriteLine(0.1m * 23.4m); // output: 2.34
+            Console.WriteLine(13 / 5);  // output: 2
+            Console.WriteLine(-13 / 5);   // output: -2
+            Console.WriteLine(13 / -5);   // output: -2
+            Console.WriteLine(-13 / -5);  // output: 2
+
+            Console.WriteLine(16.8f / 4.1f);   // output: 4.097561
+            Console.WriteLine(16.8d / 4.1d);   // output: 4.09756097560976
+            Console.WriteLine(16.8m / 4.1m);   // output: 4.09756097560975609756097560
+
+            Console.WriteLine(5 % 4);   // output: 1
+            Console.WriteLine(5 % -4);  // output: 1
+            Console.WriteLine(-5 % 4);  // output: -1
+            Console.WriteLine(-5 % -4); // output: -1
+
+            Console.WriteLine(-5.2f % 2.0f); // output: -1.2
+            Console.WriteLine(5.9 % 3.1);   // output: 2.8
+            Console.WriteLine(5.9m % 3.1m);  // output: 2.8
+
+            Console.WriteLine(5 + 4);       // output: 9
+            Console.WriteLine(5 + 4.3);     // output: 9.3
+            Console.WriteLine(5.1m + 4.2m); // output: 9.3
+
+            Console.WriteLine(47 - 3);      // output: 44
+            Console.WriteLine(5 - 4.3);     // output: 0.7
+            Console.WriteLine(7.5m - 2.3m); // output: 5.2
+
+            int x = 5;
+            x += 9;
+            Console.WriteLine(x);  // output: 14
+
+            x -= 4;
+            Console.WriteLine(x);  // output: 10
+
+            x *= 2;
+            Console.WriteLine(x);  // output: 20
+
+            x /= 4;
+            Console.WriteLine(x);  // output: 5
+
+            x %= 3;
+            Console.WriteLine(x);  // output: 2
+
+
+
         }
     }
 }
