02aa9fd012b1a5e8f6758c0ab636509898e10c68
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-25 18:12:47 +0600

    C# Classes, Object, Auto Property

---
+++ Examples/OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using OOP;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WaterBottle bottle = new WaterBottle("red", 300);
+        bottle.WaterAmount = 300;
+
+        // Add water using Method
+        bottle.AddWater(200);
+
+        //bottle.color = "red"; // produce compilation error
+
+
+        WaterBottle bottle2 = new WaterBottle("blue", 200);
+
+        // assign value using variable / field
+        //bottle2.waterAmount = 400;
+        bottle2.AddWater(100);
+
+        //bottle2.color = "blue"; // produce compilation error
+
+        // Print the water amount of the bottle
+        Console.WriteLine(bottle.WaterAmount);
+        Console.WriteLine(bottle2.WaterAmount);
+
+        // Print the color of bottle
+        //Console.WriteLine(bottle.GetColor());
+        //Console.WriteLine(bottle2.GetColor());
+
+        // Get color using property
+        Console.WriteLine(bottle.Color);
+        Console.WriteLine(bottle2.Color);
+    }
+}
---
+++ Examples/OOP/WaterBottle.cs
@@ -0,0 +1,82 @@
+﻿
+namespace OOP
+{
+    // Class name is always noun
+    public class WaterBottle
+    {
+        //private double waterAmount; // Field
+        //public double waterAmount;
+        private double waterAmount;
+        //public string color;
+        private string color;
+        private double capacity;
+        public bool IsOpen { get; set; }
+
+        // Constructor
+        public WaterBottle(string color, double capacity)
+        {
+            this.color = color;
+            if (capacity <= 100)
+            {
+                this.capacity = 100;
+            }
+            else this.capacity = capacity;
+        }
+
+        // Get color
+        //public string GetColor()
+        //{
+        //    return color;
+        //}
+
+        // Get Color using property
+        // Property
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+
+        //private void AddWater(double amount)
+        //{
+        //    waterAmount += amount;
+        //}
+
+        // Add water using method
+        public void AddWater(double amount)
+        {
+            waterAmount += amount;
+        }
+
+        // Set and get water amount using property
+        // Property
+
+        public double WaterAmount
+        {
+            get
+            {
+                if (waterAmount <= 0) return 0;
+                else if (waterAmount > capacity) return capacity;
+                else return waterAmount;
+            }
+            set
+            {
+                if (value > 0 && value < capacity)
+                {
+                    waterAmount = value;
+                }
+            }
+        }
+
+        //public bool IsOpen
+        //{
+        //    get
+        //    {
+        //        return isOpen;
+        //    }
+        //    set { isOpen = value; }
+        //}
+    }
+}
