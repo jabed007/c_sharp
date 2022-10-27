00df419ae82ce76c4c90092a54cd222e09aac20d
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-10-27 15:24:55 +0600

    Static Keyword, method, variable, class example

---
+++ Examples/Classes/AreaCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class AreaCalculator
+    {
+        public static string Name { get; set; }
+        public static int count;
+
+        public static double GetCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ Examples/Classes/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Rectangle
+    {
+        public string Name { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
