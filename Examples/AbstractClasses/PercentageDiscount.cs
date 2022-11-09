bd4869755a68baa730fbcd465ac90b6e2c04df89
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-09 10:23:45 +0600

    Abstract class and abstract method

---
+++ Examples/AbstractClasses/PercentageDiscount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public class PercentageDiscount : Discount
+    {
+        public override int AmountAfterDiscount(int totalAmount)
+        {
+            return (int)(totalAmount - (totalAmount * DiscountAmount) / 100.0);
+        }
+
+        public override int GetDiscountedAmount()
+        {
+            return base.GetDiscountedAmount();
+        }
+    }
+}
---
+++ Examples/Classes/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public readonly string idPrefix;
+
+        public Person(string idPrefix)
+        {
+            this.idPrefix = idPrefix;
+        }
+
+        public string GenarateId()
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                return $"{idPrefix}-{Name.Substring(0, 2)}";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
