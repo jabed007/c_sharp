aa3871c64a14a64862f0aab9c14305b6ec616190
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-09 22:25:14 +0600

    Structure

---
+++ Examples/Structure/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    public struct Point
+    {
+        public int X { get; set; } 
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double CalculateDistance(Point anotherPoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Examples/Structure/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 2;
+            p1.Y = 3;
+
+            Point p2;
+            p2 = p1;
+
+            p2.X = 5;
+            p2.Y = 6;
+
+            Console.WriteLine($"X: {p1.X}, Y: {p1.Y}");
+            Console.WriteLine($"X: {p2.X}, Y: {p2.Y}");
+        }
+    }
+}
