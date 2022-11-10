3cc8d4776f05771e9ff4e4ba227b2ea33137283b
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-10 12:51:06 +0600

    static method static class

---
+++ Examples/StaticClass/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public class Calculator
+    {
+        public static double PI = 3.1416;
+        public int count = 0;
+        public static double CalculateAverage(double[] numbers)
+        {
+            var sum = 0.0;
+
+            string? name = null;
+
+            foreach(var  number in numbers)
+            {
+                sum += number;
+            }
+            return sum / numbers.Length;
+        }
+
+        public double GetPI()
+        {
+            return PI;
+        }
+    }
+}
---
+++ Examples/StaticClass/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StaticClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Calculator.CalculateAverage(new double[] {2.3, 3.4});
+            Console.WriteLine(result);
+
+            Console.WriteLine(Calculator.PI);
+
+            Calculator calculator = new Calculator();
+            calculator.count = 5;
+
+            Calculator calculator2 = new Calculator();
+            calculator2.count = 9;
+
+            Calculator.PI = 4.9;
+
+            Console.WriteLine(calculator.GetPI());
+            Console.WriteLine(calculator2.GetPI());
+        }
+    }
+}
