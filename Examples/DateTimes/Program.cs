5ccc16b1fe07577cc6ced4cec000e73b60a77d6b
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-17 11:50:33 +0600

    DateTime

---
+++ Examples/DateTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DateTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = new DateTime(2021, 8, 26);
+            Console.WriteLine(today);
+
+            DateTime now = new DateTime(2021, 8, 26, 21, 25, 00);
+            Console.WriteLine(now);
+
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+
+            DateTime parsedDate = DateTime.Parse("17/11/2022");
+            Console.WriteLine(parsedDate);
+
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.AddYears(2));
+            Console.WriteLine(now.DayOfYear);
+        }
+    }
+}
