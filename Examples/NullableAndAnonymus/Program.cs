707a9ab9ed839b9f882b6a9bdc1bff51aafff0a2
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-17 21:51:14 +0600

    nullable data types

---
+++ Examples/NullableAndAnonymus/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NullableAndAnonymus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            x = 6;
+
+            string? s = null;
+
+            if (x == null)
+            {
+                Console.WriteLine("x is null");
+            }
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(x.Value);
+
+            Print(x.Value);
+         }
+
+        static void Print(int n)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
