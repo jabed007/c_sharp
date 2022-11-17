b255bce3cb85f2d3d3c0b12247add0418c0bfcd4
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-17 13:10:28 +0600

    params keyword

---
+++ Examples/ParameterModifier/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ParameterModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result = Sum(new int[] {2, 3, 4, 5, 7, 8, 9});
+            var result = Sum("Hello", 2, 3, 4, 5, 7, 8, 9);
+            Console.WriteLine(result);
+        }
+        public static int Sum(string s, params int[] nums)
+        {
+            var sum = 0;
+            foreach(var num in nums)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        static void TestingParameterModifier(int a, int b)
+        {
+
+        }
+    }
+}
