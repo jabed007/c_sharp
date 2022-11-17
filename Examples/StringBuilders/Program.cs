5963ee0c514516703819e11b2e80cb0c30bf4482
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-17 12:44:50 +0600

    string builder

---
+++ Examples/StringBuilders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace StringBuilders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            while(true)
+            {
+                var word = Console.ReadLine();
+                if (word == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    stringBuilder.Append("<p>").Append(word).AppendLine("</p>");
+                }
+            }
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
