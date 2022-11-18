f6c22c9a0003e736081c2adebd1d89fb7e797fe5
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-18 11:26:22 +0600

    example of params keyword

---
+++ Examples/ParameterModifierExample/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ParameterModifierExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double r1 = CalculateAverage(10, 20);
+            double r2 = CalculateAverage(10, 20, 30);
+
+            // Uses of params keyword
+            //double average = CalculateAverage(new double[] {1, 2, 3, 4});
+            //Console.WriteLine(average);
+
+            var average = CalculateAverage(1, 2, 3, 4, 5);
+            Console.WriteLine(average);
+        }
+        public static double CalculateAverage(double x, double y)
+        {
+            return (x + y) / 2;
+        }
+
+        public static double CalculateAverage(double x, double y, double z)
+        {
+            return (x + y + z) / 3;
+        }
+
+        public static double CalculateAverage(params double[] numbers)
+        {
+            var sum = 0.0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            
+            return sum / numbers.Length;
+        }
+    }
+}
---
+++ Examples/StringBuilderExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace StringBuilderExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = "Jatrabari" + ", Dhaka";
+            Console.WriteLine(location);
+            location += ", Bangladesh";
+            Console.WriteLine(location);
+
+            // String Builder
+            StringBuilder stringBuilder = new StringBuilder(location);
+
+            stringBuilder.Append(", South Asia");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Replace("a", "@");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Insert(40, ".");
+            Console.WriteLine(stringBuilder);
+
+            // Convert stringBuilder.ToString();
+            location = stringBuilder.ToString();
+            Console.WriteLine(location);
+
+            // String Builder can be use like array
+            for (int i = 0; i < location.Length; i++)
+            {
+                Console.WriteLine(location[i]);
+            }
+
+            // We can upadte value of string by using index number in string builder, but in general string we can not do this
+            stringBuilder[0] = 'A';
+        }
+    }
+}
