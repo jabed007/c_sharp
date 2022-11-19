1cb707064a66091c6e1fb1458b337ecda58048cf
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-11-19 19:14:20 +0600

    generics class, constraints, default keyword

--- Examples/Generics/Program.cs
+++ Examples/Generics/Program.cs
@@ -10,16 +10,76 @@
             //p.Y = 20.5;
 
             //double result = ((int)p.X) + ((double)p.Y);
+
+            // Uses of Generics class
+            Point<double> p = new Point<double>();
+            p.X = 10.5;
+            p.Y = 20.5;
+
+            var result = p.X + p.Y;
+            Console.WriteLine(result);
+
+            Point2<int, double> p2 = new Point2<int, double>();
+            p2.X = 100;
+            p2.Y = 200.50;
+
+            Tree<string> tree = new Tree<string>();
+            tree.AddNode("ABC");
+            tree.AddNode("XYZ");
+
+            string item = tree.RemoveNode("XYZ");
+            Console.WriteLine(item);
         }
     }
-    public class Point<T>
+
+    // Generics Class
+    public class Point<T> where T:struct // Single Parameterized Generics class
     {
         //public object X { get; set; }
         //public object Y { get; set; }
         public T X { get; set; }
         public T Y { get; set; }
     }
-    public class FractionPoint
+
+    public class Point2<T, D> where T:class, new() where D:struct // Multiple Parameterized Generics class
+    {
+        public T X { get; set; }
+        public D Y { get; set; }
+
+        //public S Sum()
+        //{
+        //    return X + Y; // error
+        //}
+    }
+
+    public interface Isumable
+    {
+        public string Sum(string a, string b);
+    }
+
+    public class Graph<T>
+    {
+
+    }
+
+    public class Tree<T>
+    {
+        private T[] nodes;
+        private int index = 0;
+        public void AddNode(T node)
+        {
+            nodes[index++] = node;
+        }
+
+        public T RemoveNode(T node)
+        {
+            T currentItem = nodes[--index];
+            nodes[index] = default(T);
+            return currentItem;
+        }
+    }
+
+    public class FractionPoint // Normal class
     {
         public double X { get; set; }
         public double Y { get; set; }
