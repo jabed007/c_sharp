7797ed47859897b214f9a17711ba722db25e282b
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-13 12:41:29 +0600

    Basic Syntax

---
+++ DevSkill-CSharp-Code-Example/01_Basic_Syntax/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// variable can have alphabet, digit or underscore and it must start with either alphabet or underscore.
+int student_age = 23;
+string studentName = "Md. Jabed Hossain";
+bool isAdmitted = true;
+double weight = 83.5;
+char initial = 'A';
+
+string line =  Console.ReadLine();
+
+//int size = line.Length;
+//Console.WriteLine(size);
+
+string[] lineParts = line.Split(' ');
+for (int i = 0; i < lineParts.Length; i++)
+{
+    Console.WriteLine(lineParts[i]);
+}
